65fa92555bc30fac059856584d41012fa61d194e
Oscar <oscarborden@hotmail.co.uk>
2023-09-04 14:01:33 +0100

    first java script

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // first script
+        String example = "Hello World!";
+        System.out.println(example);
+
+        // sum of two integers
+        System.out.println(sum(5.4, 9.6));
+
+    }
+    public static double sum(double a,  double b) {
+        double sum = a + b;
+        return sum;
+    }
+
+}
